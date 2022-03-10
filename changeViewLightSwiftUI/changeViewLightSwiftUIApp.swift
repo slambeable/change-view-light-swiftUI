9c041e4041d64c42935092181613678339b058eb
slambeable <41022916+slambeable@users.noreply.github.com>
2022-03-11 02:42:21 +0300

    Initial Commit

---
+++ changeViewLightSwiftUI/changeViewLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  changeViewLightSwiftUIApp.swift
+//  changeViewLightSwiftUI
+//
+//  Created by Андрей Евдокимов on 11.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct changeViewLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
